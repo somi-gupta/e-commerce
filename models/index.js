be01ac7ca7175615168c3d2a1da419273c4503f7
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-04-22 18:28:08 -0400

    Updated the relationship b/w product and tag models

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ Product.belongsTo(Category);
 Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag,{through: 'ProductTag'});
+Product.belongsToMany(Tag,{through: ProductTag});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{through: ProductTag});
